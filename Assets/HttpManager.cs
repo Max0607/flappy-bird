19847a80fe0d5c6c00372649c7d7961bff059921
Marcelo Alvarenga Tamayo <marcelo.alvarenga.tamayo@gmail.com>
2022-08-07 13:18:20 -0500

    tarea 1

--- Assets/HttpManager.cs
+++ Assets/HttpManager.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class HttpManager : MonoBehaviour
 {
 
     [SerializeField]
     private string URL;
+
+    public Text[] NameText;
+    public Text[] ScoreText;
+
+    public Animator PipeAnim;
+
+    public GameObject ScorePanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    public void ExitPipe()
+    {
+        ScorePanel.SetActive(false);
     }
 
     public void ClickGetScores()
@@ -33,10 +45,16 @@ public class HttpManager : MonoBehaviour
         else if(www.responseCode == 200){
             //Debug.Log(www.downloadHandler.text);
             Scores resData = JsonUtility.FromJson<Scores>(www.downloadHandler.text);
+            ScorePanel.SetActive(true);
 
             foreach (ScoreData score in resData.scores)
             {
                 Debug.Log(score.userId +" | "+score.value);
+                NameText[score.userId - 1].text = score.userId.ToString();
+            }
+            for (int i = 0; i < resData.scores.Length; i++)
+            {
+                ScoreText[i].text = resData.scores[i].value.ToString();
             }
         }
         else
